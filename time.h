8289c0202ba4ef81cfab1da3c40e5916b8b5d3a3
Thomas Higgs <thomas.higgs@renishaw.com>
2026-03-16 13:57:28 +0000

    Included timers to have freeze functionality - changed start time to 0

--- time.h
+++ time.h
@@ -9,10 +9,12 @@ extern volatile unsigned char *Minutes;
 extern volatile unsigned char *Hours;
 
 extern volatile int running;
+extern volatile unsigned char freezeDisplay;
 
 // Function prototypes
 void Time_Init(void);
 void Time_Increment(void);
 void Time_Reset(void);
+void Lap_Display(unsigned int h, unsigned int m, unsigned int s);
 
 #endif /* TIME_H_ */
